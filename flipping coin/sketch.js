eab1a7a35977562de12f7f1e80ee932aeab01c56
WillDrac <willdrac@yahoo.com>
2025-01-24 13:58:07 -0500

    kicking the coin off with a force

--- flipping coin/sketch.js
+++ flipping coin/sketch.js
@@ -1,13 +1,24 @@
 let camera
 
-let coinPos
-
 let coinModel
 let coinTexture
 
 let groundTexture
 
 let vectorModel
+
+
+
+let coinPos
+let coinLinVel
+let coinRotVel
+
+const coinMass = 0.0055
+const coinRadius = 0.01
+
+const worldScale = 1/coinRadius
+
+
 
 function preload() {
     coinModel = loadModel("assets/coin assets/coin.obj")
@@ -23,6 +34,7 @@ function setup() {
 
     camera = createCamera()
     camera.roll(PI)
+    camera.frustum(-width/20, width/20, -height/20, height/20, 0.1*800, 30*800)
 
     noStroke()
 
@@ -30,18 +42,32 @@ function setup() {
 
     specularMaterial(125)
 
+
+
     coinPos = createVector(0, 0, 0)
+    coinLinVel = createVector(0, 3, 0)
+
+    coinRotVel = getRotVel(
+        createVector(0, 1, 1), //force
+        createVector(0.01, 0, 0.005), //force position (make sure it's on the coin, y=0, x & z < r)
+        0.001, //time the force is applied
+        coinMass,
+        coinRadius
+    )
+    console.log(coinRotVel)
 }
 
 let t = 0
 function draw() {
     background(0)
 
-    t += deltaTime / 1000
+    const dt = deltaTime / 10000
+    t += dt
 
-    coinPos.add(createVector(0, 0.1, 0))
+    coinLinVel.add(createVector(0, -9.81 * dt, 0))
+    coinPos.add(coinLinVel.copy().mult(dt))
 
-    scale(100)
+    scale(1/coinRadius)
 
     orbitControl(15, 15, 1)
 
@@ -49,33 +75,45 @@ function draw() {
     ambientLight(150)
 
     // rotation axis of the coin
-    const axis = createVector(0, 2, 1).normalize()
+    const axis = coinRotVel.copy().normalize()
 
     shininess(100)
     texture(coinTexture)
     push()
-    applyMatrix(getRotationMatrix(axis, 2*t))
-    console.log(axis.copy().dot(createVector(0, 1, 0)))
+
+    applyMatrix(getRotationMatrix(axis, coinRotVel.mag() * t))
     model(coinModel)
+
+    rotateX(-PI / 2)
+    fill(255, 255, 0)
+    model(vectorModel)
+
     pop()
+
+
 
     fill(255, 0, 0)
     push()
-    // applyMatrix(vectorPointMatrix(createVector(Math.sin(t), Math.cos(t), 0)))
+    applyMatrix(vectorPointMatrix(axis))
     model(vectorModel)
     pop()
 
-    translate(-coinPos.x, -coinPos.y, -coinPos.z) //after this, all object will be drawn relative to the ground and not the coin position
+
+
+    translate(-coinPos.x*worldScale, -coinPos.y*worldScale, -coinPos.z*worldScale) //after this, all object will be drawn relative to the ground and not the coin position
+
+
 
     shininess(1)
     texture(groundTexture)
     push()
-    rotateX(PI/2)
+    rotateX(PI / 2)
     plane(100, 100)
     pop()
 }
 
 // https://en.wikipedia.org/wiki/Rotation_matrix#Rotation_matrix_from_axis_and_angle
+// returns a matrix that rotates a 3d object around the given axis by the given angle
 function getRotationMatrix(axis, angle) {
     let c = cos(angle)
     let s = sin(angle)
@@ -93,8 +131,22 @@ function getRotationMatrix(axis, angle) {
     ]
 }
 
+// returns a matrix that points a 3d object from +z to the given direction
 function vectorPointMatrix(direction) {
-    const originalVectorDir = createVector(0, 0, 1)
-    const YZAngle = Math.atan2(direction.y, direction.z)
-    const XtoYZAngle = Math.atan2(direction.x, sqrt(direction.y**2 + direction.z**2))
+    const rotationAxis = createVector(0, 0, 1).cross(direction).normalize()
+    const rotationAngle = -Math.acos(createVector(0, 0, 1).dot(direction))
+
+    return getRotationMatrix(rotationAxis, rotationAngle)
 }
+
+// returns the rotational velocity of the coin given a force acting on it
+function getRotVel(force, forcePosition, forceTime, coinMass, coinRadius) {
+    const torque = forcePosition.copy().cross(force)
+    const torqueXZMagNorm = (torque.x * torque.x + torque.z * torque.z) / torque.mag()
+
+    const momentOfInertia = (coinMass * coinRadius * coinRadius / 4) * (2 - torqueXZMagNorm)
+
+    return torque.mult(forceTime / momentOfInertia)
+}
+
+// https://physics.stackexchange.com/questions/268698/compute-an-objects-inertia-around-an-arbitrary-axis-using-its-known-values-for
